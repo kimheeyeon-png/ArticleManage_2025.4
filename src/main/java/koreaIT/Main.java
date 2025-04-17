b0b9d71461beb6c60fd0ac67358ef771e54d7db5
heeyeon477 <gimh64627@gmail.com>
2025-04-17 15:25:32 +0900

    게시글 상세보기 명령어 유효성 검사

--- src/main/java/koreaIT/Main.java
+++ src/main/java/koreaIT/Main.java
@@ -14,17 +14,34 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        while(true){
+        while (true) {
             System.out.print("명령어) ");
-            String cmd = sc.nextLine();
+            String cmd = sc.nextLine().trim();
 
-            if (cmd.equals("article list")){
-                System.out.println("번호  /  제목  / 내용");
-                for(int i = articleList.size()-1 ; i >= 0;i--){
-                    System.out.printf("%d   / %s    / %s\n", articleList.get(i).getId(), articleList.get(i).getTitle(),  articleList.get(i).getBody());
+            // 명령어 유효성 검사
+            if (cmd.startsWith("article detail")) {
+                // parsing start
+                String[] cmdBits = cmd.split(" ");
+
+                if (cmdBits.length > 3) {
+                    System.out.println("명령어를 제대로 입력해주세요.");
                 }
-            }
-            else if (cmd.equals("article write")){
+
+                try {
+                    int detailId = Integer.parseInt(cmdBits[2]);
+                } catch (NumberFormatException e) {
+                    System.out.println("정수를 제대로 입력해주세요.");
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("정수를 추가해서 입력해주세요.");
+                }
+                // parsing end
+
+            } else if (cmd.equals("article list")) {
+                System.out.println("번호  /  제목  / 내용");
+                for (int i = articleList.size() - 1; i >= 0; i--) {
+                    System.out.printf("%d   / %s    / %s\n", articleList.get(i).getId(), articleList.get(i).getTitle(), articleList.get(i).getBody());
+                }
+            } else if (cmd.equals("article write")) {
                 System.out.print("제목 : ");
                 String title = sc.nextLine();
                 System.out.print("내용 : ");
@@ -41,13 +58,10 @@ public class Main {
                 articleList.add(addArticle);
 
                 System.out.printf("%d번 글이 생성되었습니다.\n", id);
-            }
-
-            else if(cmd.equals("exit")){
+            } else if (cmd.equals("exit")) {
                 System.out.println("== 프로그램 종료 ==");
                 break;
-            }
-            else{
+            } else {
                 System.out.println("사용할 수 없는 명령어입니다.");
             }
 
