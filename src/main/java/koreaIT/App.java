4c1b1e74ffcaa75cff363cd16dd3a82e07564a60
heeyeon477 <gimh64627@gmail.com>
2025-04-22 20:35:43 +0900

    memberController 생성

--- src/main/java/koreaIT/App.java
+++ src/main/java/koreaIT/App.java
@@ -1,9 +1,11 @@
 package koreaIT;
 
+import koreaIT.controller.MemberController;
 import koreaIT.dto.Article;
 import koreaIT.dto.Member;
 import koreaIT.util.Util;
 
+import javax.sound.midi.MetaMessage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -24,9 +26,10 @@ public class App {
         makeTestData();
 
         int lastArticleId = 3;
-        int lastMemebrId = 0;
 
         Scanner sc = new Scanner(System.in);
+
+        MemberController memberController = new MemberController(sc, memberList);
 
         while (true) {
             System.out.print("명령어) ");
@@ -35,44 +38,7 @@ public class App {
 
             if (cmd.startsWith("member join")) {
 
-                String loginId;
-                while (true) {
-                    System.out.print("로그인 아이디 : ");
-                    loginId = sc.nextLine();
-
-                    if (isJoinableLoginId(loginId) == false) {
-                        System.out.println("이미 사용 중인 아이디입니다.");
-                        continue;
-                    }
-                    break;
-                }
-
-                String loginPw;
-                while (true) {
-                    System.out.print("로그인 비밀번호 : ");
-                    loginPw = sc.nextLine();
-                    System.out.print("로그인 비밀번호 확인 : ");
-                    String loginPwConfirm = sc.nextLine();
-
-                    if (!loginPw.equals(loginPwConfirm)) {
-                        System.out.println("비밀번호 다시 확인해주세요.");
-                        continue;
-                    }
-                    break;
-                }
-
-
-                System.out.print("이름 : ");
-                String name = sc.nextLine();
-
-                String regDate = Util.getNowDate();
-
-                lastMemebrId++;
-                Member addMember = new Member(lastMemebrId, regDate, "", loginId, loginPw, name);
-
-                memberList.add(addMember);
-
-                System.out.printf("%d번 회원이 등록되었습니다. %s님 환영합니다.\n", lastMemebrId, name);
+               memberController.doJoin();
 
 
             } else if (cmd.startsWith("article modify")) {
@@ -196,16 +162,6 @@ public class App {
 
         }
 
-    }
-
-
-    private boolean isJoinableLoginId(String loginId) {
-        for (Member member : memberList) {
-            if (member.getLoginId().equals(loginId)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     private Article getArticleById(int id) {
